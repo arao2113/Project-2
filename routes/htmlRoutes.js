747b2597b254543e55319dc3cb8d6c2a1d1c6d52
matteichler <matteichler7@gmail.com>
2018-09-15 14:01:44 -0700

    variable switched from example to part in html and api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Part.findAll({}).then(function(dbExamples) {
       res.render("index", {
         examples: dbExamples
       });
@@ -11,8 +11,8 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/part/:id", function(req, res) {
+    db.Part.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
